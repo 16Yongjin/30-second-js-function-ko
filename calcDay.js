9562c9e467e4e05cff1a17367af9e5199b08130a
16Yongjin <yongjin0802@gmail.com>
2018-10-13 14:32:09 +0900

    first commit

---
+++ calcDay.js
@@ -0,0 +1,12 @@
+const calcDay = () => {
+  const now = new Date()
+  const start = new Date(now.getFullYear(), 0, 0)
+  const ko = 1000 * 60 * 60 * 9
+  const diff = (now - start + ko) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
+  const oneDay = 1000 * 60 * 60 * 24
+  const day = Math.floor(diff / oneDay)
+
+  return day
+}
+
+module.exports = calcDay
